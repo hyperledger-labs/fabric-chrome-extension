ff2974475395865a08719a9a07206f7b456c047d
Daniel McSheehy <dsm140130@utdallas.edu>
2018-07-05 11:35:53 -0500

    Removing incorrect site and extension
    Signed-off-by: Daniel McSheehy <dsm140130@utdallas.edu>

--- site-example/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    if ((typeof fabricInterface !== 'undefined') && (fabricInterface.info === "Hyperledger Fabric Extension")) {
-        console.log('Detected Extension');
-    } else {
-        console.log('Extension not detected');
-    }
-    
-});
