d42a187bca05e6ff5f2afa34913b5b803e0808a3
Daniel McSheehy <dsm140130@utdallas.edu>
2018-08-07 11:32:43 -0500

    New ui chrome extension
    Signed-off-by: Daniel McSheehy <dsm140130@utdallas.edu>

--- fabric-chrome-extension/src/scripts/popup.js
+++ fabric-chrome-extension/src/scripts/popup.js
@@ -4,22 +4,41 @@ const getNetworkEndpoints = async() => {
             const response = await fetch(`${networkURL}/network`);
             const body = await response.json();
             console.log('Network Response: ', body);
-            let peerInput = document.getElementById('peer_url_input');
+            $('.peer_list').empty();
+            body.peers.map((peer) => prependPeer(peer));
             let ordererInput = document.getElementById('orderer_url_input');
-            // TODO: Dropdown Menu
-            peerInput.value = body.peers[0]; // Default is the first!
             ordererInput.value = body.orderers[0]; // Default is the first!
         } catch (error) {
             console.log('Error with setting network configurations');
         }
 }
 // console.log('Peer and Orderer detected: ', getNetworkEndpoints());
+const submitPeer = () => {
+    let peerInput = $('#peer_url_input').val();
+    prependPeer(peerInput);
+}
+
+const prependPeer = (peerURL) => {
+    if (peerURL.length > 0) {
+      $('<li class="peer">').text(peerURL).prependTo('.peer_list');
+      //console.log('peerInput', peerInput);
+      $('#peer_url_input').val('');
+      $('.peer').click(function() {
+        $(this).remove();
+        });
+    }
+  }
 
 const submitNetworkConfig = () => {
     try {
-        let networkURL = document.getElementById('network_url_input').value;
-        let peerURL = document.getElementById('peer_url_input').value;
-        let ordererURL = document.getElementById('orderer_url_input').value;
+        console.log('SHowing up?')
+        let networkURL = $('#network_url_input').val();
+        let ordererURL = $('#orderer_url_input').val();
+        let peerURL = [];
+        $(".peer_list").find("li").each(function(){
+        var selected_peers = $(this).text();
+        peerURL.push(selected_peers);
+        });
         chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             let message = {
                 networkURL: networkURL,
@@ -37,25 +56,31 @@ const submitNetworkConfig = () => {
 
 const clearNetworkConfigStorage = () => {
     chrome.storage.sync.remove(['networkConfig'], (results) => {
-            document.getElementById('network_url_input').value = '';
-            document.getElementById('peer_url_input').value = '';
-            document.getElementById('orderer_url_input').value = '';
+        $('#network_url_input').val('');
+        $('#peer_url_input').val('');
+        $('#network_url_input').val('');
+        $('#orderer_url_input').val('');
+        $('.peer_list').html('');
     });
 }
 
-
 document.getElementById('network_url_button').addEventListener("click", 
-    (event) => { getNetworkEndpoints() }); 
+    (event) => { getNetworkEndpoints() });
+
 
 document.getElementById('network_reset_button').addEventListener("click", 
     (event) => { clearNetworkConfigStorage() }); 
 
-document.getElementById('network_url_form').addEventListener("submit", 
+document.getElementById('add_peer_button').addEventListener("click", 
+    (event) => { submitPeer() }); 
+
+document.getElementById('submit_network_button').addEventListener("click", 
     (event) => { submitNetworkConfig() }); 
 
 // Load stored network configurations to form.
 try {
     chrome.storage.sync.get(['networkConfig'], (results) => {
+        console.log(results.networkConfig);
         if (typeof results.networkConfig.networkURL !== undefined) {
             document.getElementById('network_url_input').value = results.networkConfig.networkURL;
             document.getElementById('peer_url_input').value = results.networkConfig.peerURL;
@@ -65,6 +90,9 @@ try {
 } catch (error) {
     console.log('error with loading persisted settings');
 }
+
+
+
 
 
 
