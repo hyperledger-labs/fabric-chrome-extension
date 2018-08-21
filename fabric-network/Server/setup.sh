d057ff986e0333eb3b0a4fcfe752272bdc7c7404
Daniel McSheehy <dsm140130@utdallas.edu>
2018-08-21 16:20:51 -0500

    Adding documentation for setup script
    Signed-off-by: Daniel McSheehy <dsm140130@utdallas.edu>

--- fabric-network/Server/setup.sh
+++ fabric-network/Server/setup.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
+# Install Fabric binaries
 curl -sSL http://bit.ly/2ysbOFE | bash -s 1.2.0 1.2.0 0.4.10
 sleep 5
 
 export PATH=$PWD/bin:$PATH
 
+# start network script
 bash ./startFabric.sh
-
+# Remove user log key store before login
 rm ~/.hfc-key-store/*
 
+#node scripts for registering Admin and uers
 node registerAdmin.js
 sleep 5
 node registerUser.js
-
-
+#  Start middleware for Chrome Extension
 node server.js
